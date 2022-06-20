92a02f5972bc2a4a2186d4ef0e565eff0f00f931
Valentin Eberhardt <valentin.eberhardt571@gmail.com>
2022-06-20 16:03:55 +0200

    Update Data & Company.py

--- Structures/Company.py
+++ Structures/Company.py
@@ -1,17 +1,31 @@
+# ======================================================================== #
+# IMPORTS                                                                  #
+# ======================================================================== #
+
+
 from imports import *
+# ======================================================================== #
+# CLASS                                                                    #
+# ======================================================================== #
 
 
 class Shop:
+    """
+    Class which defines a new shop
+    """
 
-    def __init__(self, brand_name):
+    def __init__(self, brand_name: str, product_base: str) -> None:
         self.brand_name = brand_name
         self.url = ''
+        self.current_url = ''
+        self.kind_of_product = product_base
 
         # execution
         self.data_set = self.load_json_file("../Data/WebsitesFeatures.json", self.brand_name)
         self.get_url()
 
-    def load_json_file(self, file_name: str, key_arg: str) -> dict:
+    @staticmethod
+    def load_json_file(file_name: str, key_arg: str) -> dict:
         """
         Converting a json file to a dictionary
         :param file_name : str, file's name
@@ -23,7 +37,7 @@ class Shop:
 
         return data_dict[key_arg]
 
-    def get_url(self):
+    def get_url(self) -> None:
         """
         Getting the url
         :param : None
@@ -32,15 +46,17 @@ class Shop:
         url_brand = self.load_json_file('../Data/Websites.json', self.brand_name)
         self.url = url_brand
 
-    def get_response(self) -> requests.models.Response:
+    @staticmethod
+    def get_response(url: str) -> requests.models.Response:
         """
         Getting the response
-        :param : None
+        :param url: str
         :return: requests.models.Response
         """
-        return requests.get(self.url)
+        return requests.get(url)
 
-    def get_dict_keys(self, dict_to_slice, i, j) -> list:
+    @staticmethod
+    def get_dict_keys(dict_to_slice: dict, i: int, j: int) -> list:
         """
         Getting keys from index i to j
         :param dict_to_slice : list
@@ -50,14 +66,69 @@ class Shop:
         """
         return list(dict_to_slice.keys())[i:j+1]
 
-    def is_connected(self) -> bool:
+    @staticmethod
+    def get_dict_values(dict_to_slice, i, j) -> list:
+        """
+        Getting values from index i to j
+        :param dict_to_slice : list
+        :param i : int
+        :param j : int
+        :return: list
+        """
+        return list(dict_to_slice.values())[i:j+1]
+
+    def is_connected(self, url: str) -> bool:
         """
         Verify whether the request succeeded or not
-        :param : None
+        :param url : str
         :return: bool
         """
-        return self.get_response().ok
+        return self.get_response(url).ok
+
+    def get_page_content(self, url: str) -> str:
+        """
+        Display the content of a page
+        :param url : str
+        :return: str
+        """
+        return self.get_response(url).text
+
+    @staticmethod
+    def get_filters(**kwargs):
+        return kwargs
+
+    def add_features(self, features_dict: dict) -> str:
+        """
+        Adding features to the url
+        :param features_dict: dict
+        :return self.current_url : str
+        """
+        self.current_url = self.url + self.kind_of_product + "?"
+        for key, value in features_dict.items():
+            domain = self.data_set[self.kind_of_product][key]
+            for feature in value:
+                if len(feature) > 0:
+                    self.current_url += domain + feature + "|"
+
+        self.current_url = self.current_url[:-1]  # removing "|"
+        return self.current_url
 
 
-Shop = Shop('boulanger')
-kind_of_product = Shop.get_dict_keys(Shop.data_set, 0, 1)
+# ======================================================================== #
+# MAIN                                                                     #
+# ======================================================================== #
+
+
+Shop = Shop('boulanger', '/c/nav-filtre/tous-les-ordinateurs-portables')
+
+features = Shop.get_filters(
+    ssd=[""],
+    cpu=["intel20core20i5", "amd20ryzen205"],
+    ram=["820go"],
+    brand=[""],
+    hdd=[""],
+    os=["windows2011"]
+)
+
+link = Shop.add_features(features)
+print(link)
